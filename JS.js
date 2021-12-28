a460c75de4913f9c441df2e47e821842bca710a9
LindaAhmeti <linda.ahmeti3@student.uni-pr.edu>
2021-12-28 18:44:03 +0100

    Update JS.js
    Co-Authored-By: kaltrinabaliu <93614313+kaltrinabaliu@users.noreply.github.com>

--- JS.js
+++ JS.js
@@ -2,3 +2,26 @@ function ndryshoNgjyren(ngjyra)
 {
     document.body.style.backgroundColor = ngjyra;
 }
+
+function validateform(){  
+    var name=document.regjistrimi.email.value;  
+    var password=document.regjistrimi.password.value;  
+      
+    if (name==null || name==""){  
+      alert("Emri nuk duhet te jete i zbrazet.");  
+      return false;  
+    }else if(password.length<6){  
+      alert("Password duhet te jete i gjate me se shumti 6 karaktere.");  
+      return false;  
+      }  
+    }  
+
+
+    
+    function validate() {
+        let x = document.forms["forma"]["name"].value;
+        if (x == "") {
+          alert("Kjo fushe duhet plotesuar!");
+          return false;
+        }
+      }

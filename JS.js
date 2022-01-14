19eba62a7979b3dbe8355f52e5a9a8881e1a244c
kaltrinabaliu <93614313+kaltrinabaliu@users.noreply.github.com>
2022-01-14 22:41:45 +0100

    Update JS.js
    Co-Authored-By: LindaAhmeti <93882633+LindaAhmeti@users.noreply.github.com>
    Co-Authored-By: Gentiana Idrizi <93874728+Gentiana-Idrizi@users.noreply.github.com>

--- JS.js
+++ JS.js
@@ -25,3 +25,12 @@ function validateform(){
           return false;
         }
       }
+      var i = 0;
+
+      function timedCount() {
+        i = i + 1;
+        postMessage(i);
+        setTimeout("timedCount()",500);
+      }
+      
+      timedCount();
